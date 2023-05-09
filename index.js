969328e38f0a0816b4f006d6f4e39c89dca7ab19
paddy <pdd337816464@163.com>
2023-05-10 00:15:13 +0800

    修改 - 新增中间件

--- index.js
+++ index.js
@@ -1,98 +1,98 @@
 const express = require('express')
 const app = express()
 
-app.use(express.json())
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
-let persons = [
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(express.json())
+app.use(requestLogger)
+
+let notes = [
   {
     id: 1,
-    name: "Arto Hellas",
-    number: 040-123456
+    content: "HTML is easy",
+    important: true
   },
   {
     id: 2,
-    name: "Ada Lovelace",
-    number: 39-44-5323523
+    content: "Browser can execute only JavaScript",
+    important: false
   },
   {
     id: 3,
-    name: "Dan Abramov",
-    number: 12-43-234345
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: 39-23-6423122
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
   }
 ]
 
-const generateId = () => {
-  const id = Math.floor(Math.random()*100)
 
-  return id
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
 }
 
-app.get('/api/persons', (req, resp) => {
-  resp.json(persons)
-})
-
-app.get('/api/info', (req, resp) => {
-  const time = new Date()
-  resp.send('phonebook has info 2 people, <br>' + time + 'starndard time')
-})
-
-app.get('/api/persons/:id', (req, resp) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if(person) {
-    resp.status(200).json(person)
-  }
-
-  if(!person) {
-    return resp.status(404).end()
-  }
-})
-
-app.delete('/api/persons/:id', (req, resp) => {
-  const id = Number(req.params.id)
-  console.log(id, "id")
-  persons = persons.filter(person => person.id !== id)
-  
-  resp.status(204).end()
-  console.log(persons, "persons")
-})
-
-app.post('/api/persons', (request, response) => {
+app.post('/api/notes', (request, response) => {
   const body = request.body
-  console.log(body, "body")
-  if(!body.name || !body.number) {
-    return response.status(400).json({
-      error:"name or number missing"
-    }) 
-  }
-  
-  const name = body.name
-  const isPerson = persons.find(person => person.name.includes(name))
 
-  if(isPerson) {
-    return response.status(400).json({
-      error:"name must be unique"
-    }) 
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
   }
 
-  const person = {
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
     id: generateId(),
-    name: body.name,
-    number: body.number
   }
 
-  persons = persons.concat(person)
-  
-  response.json(persons)
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
-const PORT = 3002
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+  
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.use(unknownEndpoint)
+
+const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
