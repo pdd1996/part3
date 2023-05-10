7d7799858deb3c6123508d4b2d069d4208358703
paddy <pdd337816464@163.com>
2023-05-10 15:20:42 +0800

    修改 - 修改index.js

--- README.md
+++ README.md
@@ -251,3 +251,146 @@ Mogodb   WuHv9553iLRCaCBW
 #### app.use(express.json())
 
 json-parser 从请求中获取原始数据，这些数据存储在 request 对象中，将其解析为一个 JavaScript 对象，并将其作为一个新的属性 body 分配给 request 对象。
+
+#### morgan - 日志中间件
+
+```js
+const express = require('express')
+const fs = require('fs')
+const morgan = require('morgan')
+const path = require('path')
+var uuid = require('node-uuid')
+// var rfs = require('rotating-file-stream')
+
+morgan.token('id', function getId (req) {
+  return req.id
+})
+
+const app = express()
+
+// var accessLogStream = rfs.createStream("file.log", {
+//   size: "10M", // rotate every 10 MegaBytes written
+//   interval: "1d", // rotate daily
+//   compress: "gzip", // compress rotated files
+//   path: path.join(__dirname, 'log')
+// });
+// var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+function assignId (req, res, next) {
+  req.id = uuid.v4()
+  next()
+}
+
+app.use(assignId)
+app.use(morgan(':id :method :url :response-time'))
+
+app.use(express.json())
+app.use(requestLogger)
+// app.use(morgan('combined', { stream: accessLogStream }))
+
+// log only 4xx and 5xx responses to console
+app.use(morgan('dev', {
+  skip: function (req, res) { return res.statusCode < 400 }
+}))
+
+// log all requests to access.log
+app.use(morgan('common', {
+  stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+}))
+
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+  
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.use(unknownEndpoint)
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+```
+
--- index.js
+++ index.js
@@ -1,100 +1,72 @@
-const express = require('express')
-const fs = require('fs')
-const morgan = require('morgan')
-const path = require('path')
-var uuid = require('node-uuid')
-// var rfs = require('rotating-file-stream')
-
-morgan.token('id', function getId (req) {
-  return req.id
-})
-
+const express = require("express")
+const cors = require('cors')
 const app = express()
 
-// var accessLogStream = rfs.createStream("file.log", {
-//   size: "10M", // rotate every 10 MegaBytes written
-//   interval: "1d", // rotate daily
-//   compress: "gzip", // compress rotated files
-//   path: path.join(__dirname, 'log')
-// });
-// var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
-
-
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-function assignId (req, res, next) {
-  req.id = uuid.v4()
-  next()
-}
-
-app.use(assignId)
-app.use(morgan(':id :method :url :response-time'))
-
 app.use(express.json())
-app.use(requestLogger)
-// app.use(morgan('combined', { stream: accessLogStream }))
-
-// log only 4xx and 5xx responses to console
-app.use(morgan('dev', {
-  skip: function (req, res) { return res.statusCode < 400 }
-}))
-
-// log all requests to access.log
-app.use(morgan('common', {
-  stream: fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
-}))
-
+app.use(cors())
+app.use(express.static('build'))
 
 let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+    {
+      id: 1,
+      content: "HTML is easy",
+      date: "2022-05-30T17:30:31.098Z",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only Javascript",
+      date: "2022-05-30T18:39:34.091Z",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      date: "2022-05-30T19:20:14.298Z",
+      important: true
+    }
+  ]
 
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+app.get('/', (req, resp) => {
+    resp.send('<h1>Hello Node</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/notes', (req, resp) => {
+    resp.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if(note) {
+    response.json(note)
+  }
+
+  if(!note) {
+    response.status(404).end()
+  }
+  
+})
+
+app.delete('/api/notes/:id', (req, resp) => {
+  const id = Number(req.params.id)
+  notes = notes.filter(note => note.id !== id)
+  resp.status(204).end()
 })
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
+  const maxId = notes.length > 0 ? Math.max(...notes.map(note => note.id)) : 0
+
   return maxId + 1
 }
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
+  if(!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
     })
   }
 
@@ -102,36 +74,15 @@ app.post('/api/notes', (request, response) => {
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
+    id: generateId()
   }
-
-  notes = notes.concat(note)
+ 
+  notes.concat(note)
 
   response.json(note)
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-  
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
-app.use(unknownEndpoint)
-
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
